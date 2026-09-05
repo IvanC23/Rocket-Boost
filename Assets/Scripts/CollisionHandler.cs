d559eab11350d11719dc81c124fd4ac0b8b4e49a
Ivan <92627438+IvanC23@users.noreply.github.com>
2026-09-05 12:34:39 +0200

    Collisions, Reload level

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("You finished the level!");
+                break;
+            case "Fuel":
+                Debug.Log("You hit some fuel");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
